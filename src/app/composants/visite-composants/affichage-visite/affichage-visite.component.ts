10af839e2f88f1d6aaad4f1d99047dba1c839434
lise houblon <lisehoublon@gmail.com>
2020-05-15 11:19:05 +0200

    affichage visites

--- src/app/composants/visite-composants/affichage-visite/affichage-visite.component.ts
+++ src/app/composants/visite-composants/affichage-visite/affichage-visite.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { VisiteService } from 'src/app/services/visite-services/visite.service';
+import { Visite } from 'src/app/modeles/visite-modele/visite.modele';
 
 @Component({
   selector: 'app-affichage-visite',
@@ -7,9 +10,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AffichageVisiteComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router, private visiteService : VisiteService, private activatedRoute: ActivatedRoute) { }
 
+
+  /*============== PROPS ==================== */
+  visite : Visite = {idVisite:null,
+    dateDeVisite:null,
+    heureDeVisite:null,
+    bienImmobilier:null,
+    client:null,
+    conseiller:null}
+  id : number
+
+
+  /*============== METHODES ==================== */
   ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe(
+      (parameterMap)=>{
+        //recup du param id de l'url (ref : route  de app-routing.module.ts)
+        this.id = +parameterMap.get("id");
+      })
+
+      this.getById();
   }
+
+  async getById(){
+    this.visite= await this.getVisite(this.id).toPromise()
+    console.log(this.visite)
+    }
+
+  getVisite(id:number){
+    return this.visiteService.getByIdVisiteFromWsRest(id)
+  }
+
+  editVisite(id :number){
+    this.router.navigate(['compte/save-visite/', id]);
+
+  }//End editProprietaire()
+
+
+  async supprimer(id:number){
+    const suppr = await this.delete(id).toPromise()
+    console.log(suppr)
+    this.router.navigate(['compte/liste-visites']);
+    console.log("proprio supprimé")
+  }
+
+  delete(id:number){
+    return this.visiteService.deleteVisiteWithWsRest(id)
+  }
+
+  
 
 }
